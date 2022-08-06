527aabe896b073f6cb3ba20dcec3a002f5067082
Sage Phillips <sagephillips@superbazooka.com>
2022-08-06 12:00:06 -0500

    Initial commit for Jonas Schmedtman's HTML/CSS course

---
+++ src/main/java/com/example/jonasweb/JonasWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.jonasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonasWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonasWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jonasweb/JonasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jonasweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonasWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
